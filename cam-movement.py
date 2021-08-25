0c77c8f3a31d0693260492e0dd909b9cdb6a814e
FlashStopFall <bronchoian@icloud.com>
2021-08-25 13:33:10 -0500

    small changes and added fps

--- cam-movement.py
+++ cam-movement.py
@@ -5,163 +5,19 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 import math
-
-def tex_coord(x, y, n=4):
-    """ Return the bounding vertices of the texture square.
-
-    """
-    m = 1.0 / n
-    dx = x * m
-    dy = y * m
-    return dx, dy, dx + m, dy, dx + m, dy + m, dx, dy + m
+import functions
+import sys
 
 
-def tex_coords(top, bottom, side):
-    """ Return a list of the texture squares for the top, bottom and side.
-
-    """
-    top = tex_coord(*top)
-    bottom = tex_coord(*bottom)
-    side = tex_coord(*side)
-    result = [
-        (top),
-        (bottom),
-        (side),
-        (side),
-        (side),
-        (side),
-    ]
-    """result = []
-    result.extend(top)
-    result.extend(bottom)
-    result.extend(side * 4)"""
-    return result
 
 #block type names and location on template go here
-BLOCK1 = tex_coords((3, 0), (3, 0), (3, 0))
-
-def verts(x, y, z, n):
-    vertices = (
-        (1+(2*x), -1+(2*y), -1+(2*z)),
-        (1+(2*x), 1+(2*y), -1+(2*z)),
-        (-1+(2*x), 1+(2*y), -1+(2*z)),
-        (-1+(2*x), -1+(2*y), -1+(2*z)),
-        (1+(2*x), -1+(2*y), 1+(2*z)),
-        (1+(2*x), 1+(2*y), 1+(2*z)),
-        (-1+(2*x), -1+(2*y), 1+(2*z)),
-        (-1+(2*x), 1+(2*y), 1+(2*z))
-        )
-    return(vertices)
-
-print(verts(0, 0, 0, 1))
-
-
-
-edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
-
-colors = (
-    (1,0,0),
-    (0,1,0),
-    (0,0,1),
-    (0,1,0),
-    (1,1,1),
-    (0,1,1),
-    (1,0,0),
-    (0,1,0),
-    (0,0,1),
-    (1,0,0),
-    (1,1,1),
-    (0,1,1),
-    )
-
-surfaces = (
-    (0,1,2,3),
-    (3,2,7,6),
-    (6,7,5,4),
-    (4,5,1,0),
-    (1,5,7,2),
-    (4,0,3,6)
-    )
-
-
-forced = False
-def Cube(vx,vy,vz,block):
-    if not forced:
-        glBegin(GL_QUADS)
-        y = 0
-        for surface in surfaces:
-            x = 0
-            y+=1
-            for vertex in surface:
-                x+=1
-                #glColor3fv(colors[x])
-                glTexCoord2f(block[y-1][2*(x-1)], block[y-1][(2*x)-1])
-                #print(block[y-1][2*(x-1)], block[y-1][(2*x)-1])
-                glVertex3fv(verts(vx,vy,vz,1)[vertex])
-        glEnd()
-
-
-        
-        glBegin(GL_LINES)
-        for edge in edges:
-            for vertex in edge:
-                glVertex3fv(verts(vx,vy,vz,1)[vertex])
-        glEnd()
-    else:
-        texX = 0.75
-        texY = 0.25
-        glBegin(GL_QUADS)
-        glTexCoord2f(0.0+texX, 0.0)
-        glVertex3f(-1.0, -1.0,  1.0)
-        glTexCoord2f(0.25+texX, 0.0)
-        glVertex3f(1.0, -1.0,  1.0)
-        glTexCoord2f(0.25+texX, 0.25)
-        glVertex3f(1.0,  1.0,  1.0)
-        glTexCoord2f(0.0+texX, 0.25)
-        glVertex3f(-1.0,  1.0,  1.0)
-        glEnd()
-
-def loadTexture():
-    textureSurface = pygame.image.load('texture2.png')
-    textureData = pygame.image.tostring(textureSurface, "RGBA", 1)
-    width = textureSurface.get_width()
-    height = textureSurface.get_height()
-
-    glColor3f(0.5, 0.5, 0.5)
-    glEnable(GL_TEXTURE_2D)
-    texid = glGenTextures(1)
-
-    glBindTexture(GL_TEXTURE_2D, texid)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height,
-                 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData)
-
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-
-    return texid
-
-    glDisable(GL_TEXTURE_2D)
-
+BLOCK1 = functions.tex_coords((3, 0), (3, 0), (3, 0))
 
 
 pygame.init()
 display = (800, 600)
-scree = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+scree = pygame.display.set_mode(display, RESIZABLE | DOUBLEBUF | OPENGL)
+clock = pygame.time.Clock()
 
 
 glEnable(GL_DEPTH_TEST)
@@ -207,7 +63,7 @@ displayCenter = [scree.get_size()[i] // 2 for i in range(2)]
 mouseMove = [0, 0]
 pygame.mouse.set_pos(displayCenter)
 
-loadTexture()
+functions.loadTexture()
 
 up_down_angle = 0.0
 paused = False
@@ -272,26 +128,29 @@ while run:
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
+        scree.blit(functions.update_fps(), (10,0))
+
         glPushMatrix()
 
+        glEnable(GL_CULL_FACE)
         glCullFace(GL_FRONT)
         glEnable(GL_TEXTURE_2D)
-        Cube(0,0,0,BLOCK1)
-        Cube(1,0,0,BLOCK1)
-        Cube(0,1,0,BLOCK1)
-        Cube(0,0,1,BLOCK1)
-        Cube(-2,0,0,BLOCK1)
+        functions.Cube(0,0,0,BLOCK1)
+        functions.Cube(1,0,0,BLOCK1)
+        functions.Cube(0,1,0,BLOCK1)
+        functions.Cube(0,0,1,BLOCK1)
+        functions.Cube(-2,0,0,BLOCK1)
 
         glDisable(GL_CULL_FACE)
         glDisable(GL_TEXTURE_2D)
         glColor4f(0.5, 0.5, 0.5, 1)
         glBegin(GL_QUADS)
-        glVertex3f(-10, -10, -2)
-        glVertex3f(10, -10, -2)
-        glVertex3f(10, 10, -2)
-        glVertex3f(-10, 10, -2)
+        glVertex3f(-10, -10, -1)
+        glVertex3f(10, -10, -1)
+        glVertex3f(10, 10, -1)
+        glVertex3f(-10, 10, -1)
         glEnd()
-
+        """
         glEnable(GL_CULL_FACE)
         glCullFace(GL_BACK)
         glTranslatef(-1.5, 0, 0)
@@ -301,11 +160,14 @@ while run:
         glTranslatef(3, 0, 0)
         glColor4f(0.2, 0.2, 0.5, 1)
         gluSphere(sphere, 1.0, 32, 16)
+        """
         glColor3f(1, 1, 1)
 
         glPopMatrix()
-
+        
+        clock.tick(60)
         pygame.display.flip()
+        print(clock.get_fps())
         pygame.time.wait(10)
 
 pygame.quit()
---
+++ functions.py
@@ -0,0 +1,170 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+def tex_coord(x, y, n=4):
+    """ Return the bounding vertices of the texture square.
+
+    """
+    m = 1.0 / n
+    dx = x * m
+    dy = y * m
+    return dx, dy, dx + m, dy, dx + m, dy + m, dx, dy + m
+
+
+def tex_coords(top, bottom, side):
+    """ Return a list of the texture squares for the top, bottom and side.
+
+    """
+    top = tex_coord(*top)
+    bottom = tex_coord(*bottom)
+    side = tex_coord(*side)
+    result = [
+        (top),
+        (bottom),
+        (side),
+        (side),
+        (side),
+        (side),
+    ]
+    """result = []
+    result.extend(top)
+    result.extend(bottom)
+    result.extend(side * 4)"""
+    return result
+
+
+#block type names and location on template go here
+BLOCK1 = tex_coords((3, 0), (3, 0), (3, 0))
+
+
+def verts(x, y, z, n):
+    vertices = (
+        (1+(2*x), -1+(2*y), -1+(2*z)),
+        (1+(2*x), 1+(2*y), -1+(2*z)),
+        (-1+(2*x), 1+(2*y), -1+(2*z)),
+        (-1+(2*x), -1+(2*y), -1+(2*z)),
+        (1+(2*x), -1+(2*y), 1+(2*z)),
+        (1+(2*x), 1+(2*y), 1+(2*z)),
+        (-1+(2*x), -1+(2*y), 1+(2*z)),
+        (-1+(2*x), 1+(2*y), 1+(2*z))
+        )
+    return(vertices)
+
+print(verts(0, 0, 0, 1))
+
+
+
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7)
+    )
+
+colors = (
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (0,1,0),
+    (1,1,1),
+    (0,1,1),
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (1,0,0),
+    (1,1,1),
+    (0,1,1),
+    )
+
+surfaces = (
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6)
+    )
+
+
+forced = False
+def Cube(vx,vy,vz,block):
+    if not forced:
+        glBegin(GL_QUADS)
+        y = 0
+        for surface in surfaces:
+            x = 0
+            y+=1
+            for vertex in surface:
+                x+=1
+                #glColor3fv(colors[x])
+                glTexCoord2f(block[y-1][2*(x-1)], block[y-1][(2*x)-1])
+                #print(block[y-1][2*(x-1)], block[y-1][(2*x)-1])
+                glVertex3fv(verts(vx,vy,vz,1)[vertex])
+        glEnd()
+
+
+        
+        glBegin(GL_LINES)
+        for edge in edges:
+            for vertex in edge:
+                glVertex3fv(verts(vx,vy,vz,1)[vertex])
+        glEnd()
+    else:
+        texX = 0.75
+        texY = 0.25
+        glBegin(GL_QUADS)
+        glTexCoord2f(0.0+texX, 0.0)
+        glVertex3f(-1.0, -1.0,  1.0)
+        glTexCoord2f(0.25+texX, 0.0)
+        glVertex3f(1.0, -1.0,  1.0)
+        glTexCoord2f(0.25+texX, 0.25)
+        glVertex3f(1.0,  1.0,  1.0)
+        glTexCoord2f(0.0+texX, 0.25)
+        glVertex3f(-1.0,  1.0,  1.0)
+        glEnd()
+
+def loadTexture():
+    textureSurface = pygame.image.load('texture2.png')
+    textureData = pygame.image.tostring(textureSurface, "RGBA", 1)
+    width = textureSurface.get_width()
+    height = textureSurface.get_height()
+
+    glColor3f(0.5, 0.5, 0.5)
+    glEnable(GL_TEXTURE_2D)
+    texid = glGenTextures(1)
+
+    glBindTexture(GL_TEXTURE_2D, texid)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height,
+                 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData)
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+
+    return texid
+
+    glDisable(GL_TEXTURE_2D)
+
+    
+pygame.font.init()
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("Arial", 18)
+
+
+def update_fps():
+	fps = str(int(clock.get_fps()))
+	fps_text = font.render(fps, 1, pygame.Color("coral"))
+	return fps_text
+
